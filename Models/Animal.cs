9c509771efcd09cc017723e1cb2e23f6bf710047
Дмитрий Мезенцев <dmitriy@MacBook-Air-Dmitrij.local>
2023-05-16 13:59:44 +0500

    create model animal

---
+++ Models/Animal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace pis.Models
+{
+	public class Animal
+	{
+        public int RegistrationNumber { get; set; }
+
+        public string Locality { get; set; } = "";
+
+        public string AnimalCategory { get; set; } = "";
+
+        public string Gender { get; set; } = "";
+
+        public int YearOfBirth { get; set; }
+
+        public int ElectronicChipNumber { get; set; }
+
+        public string AnimalName { get; set; } = "";
+
+        public string Photos { get; set; } = "";
+
+        public string SpecialSigns { get; set; } = "";
+
+
+        public Animal(int id, string locality, string animalCategory, string gender, int yearOfBirth, int electronicChipNumber, string animalName, string photos, string specialSigns)
+		{
+            RegistrationNumber = id;
+            Locality = locality;
+            AnimalCategory = animalCategory;
+            Gender = gender;
+            YearOfBirth = yearOfBirth;
+            ElectronicChipNumber = electronicChipNumber;
+            AnimalName = animalName;
+            Photos = photos;
+            SpecialSigns = specialSigns;
+        }
+
+        public Animal()
+        {
+            
+        }
+    }
+}
+
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace pis.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
