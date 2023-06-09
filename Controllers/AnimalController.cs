ba4cb79839795fdc66518ae5e068f383c0a71acd
mr-dit <mezentsev000@mail.ru>
2023-06-09 22:33:57 +0500

    add vaccine from animal

--- Controllers/AnimalController.cs
+++ Controllers/AnimalController.cs
@@ -15,10 +15,11 @@ public class AnimalController : Controller
         _logger = logger;
     }
 
-    public IActionResult OpensRegister(string filterField, string? filterValue, string sortBy, bool isAscending, int pageNumber = 1, int pageSize = 10)
+    public IActionResult OpensRegister(string filterField, string? filterValue, string sortBy, bool isAscending,
+        int pageNumber = 1, int pageSize = 10)
     {
         filterValue = filterValue?.ToLower();
-        
+
         var animals = AnimalService.GetAnimals(filterField, filterValue, sortBy, isAscending, pageNumber, pageSize);
         var totalItems = AnimalService.GetTotalAnimals(filterField, filterValue);
         var totalPages = (int)Math.Ceiling((double)totalItems / pageSize);
@@ -31,7 +32,7 @@ public class AnimalController : Controller
         ViewBag.PageSize = pageSize;
         ViewBag.TotalItems = totalItems;
         ViewBag.TotalPages = totalPages;
-        
+
         return View(animals);
     }
 
@@ -47,17 +48,21 @@ public class AnimalController : Controller
         bool status = AnimalService.FillData(animal);
         if (status)
         {
-            return RedirectToAction("OpensRegister", new { filterField = ViewBag.FilterField, filterValue = ViewBag.FilterValue, sortBy = ViewBag.SortBy, isAscending = ViewBag.IsAscending, pageNumber = ViewBag.PageNumber, pageSize = ViewBag.PageSize });
+            return RedirectToAction("OpensRegister",
+                new
+                {
+                    filterField = ViewBag.FilterField, filterValue = ViewBag.FilterValue, sortBy = ViewBag.SortBy,
+                    isAscending = ViewBag.IsAscending, pageNumber = ViewBag.PageNumber, pageSize = ViewBag.PageSize
+                });
         }
         else
         {
             return Error();
         }
-        
     }
 
     [HttpPost]
-    public IActionResult DeleteEntry(int? id) 
+    public IActionResult DeleteEntry(int? id)
     {
         if (id != null)
         {
@@ -66,11 +71,17 @@ public class AnimalController : Controller
             if (status)
             {
                 Console.WriteLine("Объект Animal удален.");
-                return RedirectToAction("OpensRegister", new { filterField = ViewBag.FilterField, filterValue = ViewBag.FilterValue, sortBy = ViewBag.SortBy, isAscending = ViewBag.IsAscending, pageNumber = ViewBag.PageNumber, pageSize = ViewBag.PageSize });
-            }  
+                return RedirectToAction("OpensRegister",
+                    new
+                    {
+                        filterField = ViewBag.FilterField, filterValue = ViewBag.FilterValue, sortBy = ViewBag.SortBy,
+                        isAscending = ViewBag.IsAscending, pageNumber = ViewBag.PageNumber, pageSize = ViewBag.PageSize
+                    });
+            }
+
             return Error();
-            
         }
+
         return NotFound();
     }
 
@@ -80,27 +91,30 @@ public class AnimalController : Controller
         if (id != null)
         {
             var animal = AnimalService.GetEntry((int)id);
-    
-             return View(animal);
+
+            return View(animal);
         }
+
         return NotFound();
     }
-    
+
     [HttpPost]
     public async Task<IActionResult> ChangeEntry(Animal animal)
     {
-        
         bool status = AnimalService.ChangeEntry(animal);
         if (status)
         {
-            return RedirectToAction("OpensRegister", new { filterField = ViewBag.FilterField, filterValue = ViewBag.FilterValue, sortBy = ViewBag.SortBy, isAscending = ViewBag.IsAscending, pageNumber = ViewBag.PageNumber, pageSize = ViewBag.PageSize });
+            return RedirectToAction("OpensRegister",
+                new
+                {
+                    filterField = ViewBag.FilterField, filterValue = ViewBag.FilterValue, sortBy = ViewBag.SortBy,
+                    isAscending = ViewBag.IsAscending, pageNumber = ViewBag.PageNumber, pageSize = ViewBag.PageSize
+                });
         }
-        else
-        {
-            return NotFound();
-        }
+
+        return NotFound();
     }
-    
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
@@ -108,4 +122,3 @@ public class AnimalController : Controller
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
-
