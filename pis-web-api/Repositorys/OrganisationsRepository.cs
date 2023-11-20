1bd64af76e3e61eded27e55d1cb4b26b7093be11
MKstii <kabi31@yandex.ru>
2023-11-21 00:25:41 +0500

    Добавил фильтрацию по ролям в реестре организаций

--- pis-web-api/Repositorys/OrganisationsRepository.cs
+++ pis-web-api/Repositorys/OrganisationsRepository.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Diagnostics.Contracts;
 using Microsoft.EntityFrameworkCore;
 using NUnit.Framework.Constraints;
 using pis_web_api.Models.db;
+using pis_web_api.Models.post;
 using pis_web_api.Repositorys;
 
 namespace pis.Repositorys
@@ -16,16 +18,57 @@ namespace pis.Repositorys
 
         private (List<Organisation>, int) GetOrganisationsByValue(Func<Organisation, bool> value, int pageNumber, int pageSize, string sortBy, bool isAscending)
         {
-            using (Context db = new Context())
-            {
-                var allUser = db.Organisations
-                    .Include(x => x.Locality)
-                    .Include(x => x.OrgType)
-                    .Where(value)
-                    .SortBy(sortBy, isAscending);
-                var users = allUser.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
-                return (users, allUser.Count());
-            }
+            var allUser = db.Organisations
+                .Include(x => x.Locality)
+                .Include(x => x.OrgType)
+                .Where(value)
+                .SortBy(sortBy, isAscending);
+            var users = allUser.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+            return (users, allUser.Count());
+        }
+
+        private (List<Organisation>, int) GetOrganisationsByValue(Func<Organisation, bool> value, int pageNumber, int pageSize,
+            string sortBy, bool isAscending, UserPost user)
+        {
+            var orgsIds = db.Contracts
+                .Where(x => x.CustomerId == user.OrganisationId || x.PerformerId == user.OrganisationId)
+                .Select(x => x.CustomerId == user.OrganisationId ? x.PerformerId : x.CustomerId)
+                .Distinct();
+
+            var allUser = db.Organisations
+                .Where(x => orgsIds.Contains(x.OrgId))
+                .Include(x => x.Locality)
+                .Include(x => x.OrgType)
+                .Where(value)
+                .SortBy(sortBy, isAscending);
+            var users = allUser.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+            return (users, allUser.Count());
+        }
+
+        private (List<Organisation>, int) GetOrganisationsByValueForOperatorVetService(Func<Organisation, bool> value, int pageNumber, int pageSize, string sortBy, bool isAscending)
+        {
+            var orgsTypesIds = new List<int>() { 1, 2, 6 };
+            var allUser = db.Organisations
+                .Where(x => orgsTypesIds.Contains(x.OrgTypeId))
+                .Include(x => x.Locality)
+                .Include(x => x.OrgType)
+                .Where(value)
+                .SortBy(sortBy, isAscending);
+            var users = allUser.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+            return (users, allUser.Count());
+        }
+
+        private (List<Organisation>, int) GetOrganisationsByValueForOperatorOMSU(Func<Organisation, bool> value, int pageNumber, int pageSize, string sortBy, bool isAscending)
+        {
+            var orgsTypesIds = new List<int>() { 3, 4, 11, 5, 6, 8, 9, 10 };
+            var allUser = db.Organisations
+                .Where(x => orgsTypesIds.Contains(x.OrgTypeId))
+                .Include(x => x.Locality)
+                .Include(x => x.OrgType)
+                .Where(value)
+                .SortBy(sortBy, isAscending);
+            var users = allUser.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+            return (users, allUser.Count());
         }
 
         public (List<Organisation>, int) GetOrganisationsByDefault(
@@ -34,11 +77,51 @@ namespace pis.Repositorys
            GetOrganisationsByValue(org => { return true; },
                pageNumber, pageSize, sortBy, isAscending);
 
+        public (List<Organisation>, int) GetOrganisationsByDefault(
+           string value, int pageNumber,
+           int pageSize, string sortBy, bool isAscending, UserPost user) =>
+           GetOrganisationsByValue(org => { return true; },
+               pageNumber, pageSize, sortBy, isAscending, user);
+
+        public (List<Organisation>, int) GetOrganisationsByDefaultForOperatorVetService(
+           string value, int pageNumber,
+           int pageSize, string sortBy, bool isAscending) =>
+           GetOrganisationsByValueForOperatorVetService(org => { return true; },
+               pageNumber, pageSize, sortBy, isAscending);
+
+        public (List<Organisation>, int) GetOrganisationsByDefaultForOperatorOMSU(
+           string value, int pageNumber,
+           int pageSize, string sortBy, bool isAscending) =>
+           GetOrganisationsByValueForOperatorOMSU(org => { return true; },
+               pageNumber, pageSize, sortBy, isAscending);
+
+
+
         public (List<Organisation>, int) GetOrganisationsByOrgName(
            string name, int pageNumber,
            int pageSize, string sortBy, bool isAscending) =>
            GetOrganisationsByValue(org => org.OrgName.Contains(name, StringComparison.InvariantCultureIgnoreCase),
                pageNumber, pageSize, sortBy, isAscending);
+
+        public (List<Organisation>, int) GetOrganisationsByOrgName(
+           string name, int pageNumber,
+           int pageSize, string sortBy, bool isAscending, UserPost user) =>
+           GetOrganisationsByValue(org => org.OrgName.Contains(name, StringComparison.InvariantCultureIgnoreCase),
+               pageNumber, pageSize, sortBy, isAscending, user);
+
+        public (List<Organisation>, int) GetOrganisationsByOrgNameForOperatorVetService(
+           string name, int pageNumber,
+           int pageSize, string sortBy, bool isAscending) =>
+           GetOrganisationsByValueForOperatorVetService(org => org.OrgName.Contains(name, StringComparison.InvariantCultureIgnoreCase),
+               pageNumber, pageSize, sortBy, isAscending);
+
+        public (List<Organisation>, int) GetOrganisationsByOrgNameForOperatorOMSU(
+           string name, int pageNumber,
+           int pageSize, string sortBy, bool isAscending) =>
+           GetOrganisationsByValueForOperatorOMSU(org => org.OrgName.Contains(name, StringComparison.InvariantCultureIgnoreCase),
+               pageNumber, pageSize, sortBy, isAscending);
+
+
 
         public (List<Organisation>, int) GetOrganisationsByINN(
            string inn, int pageNumber,
@@ -46,11 +129,51 @@ namespace pis.Repositorys
            GetOrganisationsByValue(org => org.INN.Contains(inn, StringComparison.InvariantCultureIgnoreCase),
                pageNumber, pageSize, sortBy, isAscending);
 
+        public (List<Organisation>, int) GetOrganisationsByINN(
+           string inn, int pageNumber,
+           int pageSize, string sortBy, bool isAscending, UserPost user) =>
+           GetOrganisationsByValue(org => org.INN.Contains(inn, StringComparison.InvariantCultureIgnoreCase),
+               pageNumber, pageSize, sortBy, isAscending, user);
+
+        public (List<Organisation>, int) GetOrganisationsByINNForOperatorVetService(
+           string inn, int pageNumber,
+           int pageSize, string sortBy, bool isAscending) =>
+           GetOrganisationsByValueForOperatorVetService(org => org.INN.Contains(inn, StringComparison.InvariantCultureIgnoreCase),
+               pageNumber, pageSize, sortBy, isAscending);
+
+        public (List<Organisation>, int) GetOrganisationsByINNForOperatorOMSU(
+           string inn, int pageNumber,
+           int pageSize, string sortBy, bool isAscending) =>
+           GetOrganisationsByValueForOperatorOMSU(org => org.INN.Contains(inn, StringComparison.InvariantCultureIgnoreCase),
+               pageNumber, pageSize, sortBy, isAscending);
+
+
+
         public (List<Organisation>, int) GetOrganisationsByKPP(
            string kpp, int pageNumber,
            int pageSize, string sortBy, bool isAscending) =>
            GetOrganisationsByValue(org => org.KPP.Contains(kpp, StringComparison.InvariantCultureIgnoreCase),
                pageNumber, pageSize, sortBy, isAscending);
+
+        public (List<Organisation>, int) GetOrganisationsByKPP(
+           string kpp, int pageNumber,
+           int pageSize, string sortBy, bool isAscending, UserPost user) =>
+           GetOrganisationsByValue(org => org.KPP.Contains(kpp, StringComparison.InvariantCultureIgnoreCase),
+               pageNumber, pageSize, sortBy, isAscending, user);
+
+        public (List<Organisation>, int) GetOrganisationsByKPPForOperatorVetService(
+           string kpp, int pageNumber,
+           int pageSize, string sortBy, bool isAscending) =>
+           GetOrganisationsByValueForOperatorVetService(org => org.KPP.Contains(kpp, StringComparison.InvariantCultureIgnoreCase),
+               pageNumber, pageSize, sortBy, isAscending);
+
+        public (List<Organisation>, int) GetOrganisationsByKPPForOperatorOMSU(
+           string kpp, int pageNumber,
+           int pageSize, string sortBy, bool isAscending) =>
+           GetOrganisationsByValueForOperatorOMSU(org => org.KPP.Contains(kpp, StringComparison.InvariantCultureIgnoreCase),
+               pageNumber, pageSize, sortBy, isAscending);
+
+
 
         public (List<Organisation>, int) GetOrganisationsByLocality(
            string localityName, int pageNumber,
@@ -58,82 +181,50 @@ namespace pis.Repositorys
            GetOrganisationsByValue(org => org.Locality.NameLocality.Contains(localityName, StringComparison.InvariantCultureIgnoreCase),
                pageNumber, pageSize, sortBy, isAscending);
 
+        public (List<Organisation>, int) GetOrganisationsByLocality(
+           string localityName, int pageNumber,
+           int pageSize, string sortBy, bool isAscending, UserPost user) =>
+           GetOrganisationsByValue(org => org.Locality.NameLocality.Contains(localityName, StringComparison.InvariantCultureIgnoreCase),
+               pageNumber, pageSize, sortBy, isAscending, user);
+
+        public (List<Organisation>, int) GetOrganisationsByLocalityForOperatorVetService(
+           string localityName, int pageNumber,
+           int pageSize, string sortBy, bool isAscending) =>
+           GetOrganisationsByValueForOperatorVetService(org => org.Locality.NameLocality.Contains(localityName, StringComparison.InvariantCultureIgnoreCase),
+               pageNumber, pageSize, sortBy, isAscending);
+
+        public (List<Organisation>, int) GetOrganisationsByLocalityForOperatorOMSU(
+           string localityName, int pageNumber,
+           int pageSize, string sortBy, bool isAscending) =>
+           GetOrganisationsByValueForOperatorOMSU(org => org.Locality.NameLocality.Contains(localityName, StringComparison.InvariantCultureIgnoreCase),
+               pageNumber, pageSize, sortBy, isAscending);
+
+
+
+
         public (List<Organisation>, int) GetOrganisationsByAdressReg(
            string adressReg, int pageNumber,
            int pageSize, string sortBy, bool isAscending) =>
            GetOrganisationsByValue(org => org.AdressReg.Contains(adressReg, StringComparison.InvariantCultureIgnoreCase),
                pageNumber, pageSize, sortBy, isAscending);
 
-        //public static List<Organisation> GetOrganisations(int limit)
-        //{
-        //    using (var db = new Context())
-        //    {
-        //        var organisations = db.Organisations
-        //            .Take(limit)
-        //            .Include(x => x.OrgType)
-        //            .Include(x => x.Locality)
-        //            .ToList();
-        //        return organisations;
-        //    }
-        //}
+        public (List<Organisation>, int) GetOrganisationsByAdressReg(
+           string adressReg, int pageNumber,
+           int pageSize, string sortBy, bool isAscending, UserPost user) =>
+           GetOrganisationsByValue(org => org.AdressReg.Contains(adressReg, StringComparison.InvariantCultureIgnoreCase),
+               pageNumber, pageSize, sortBy, isAscending, user);
 
-        //public static Organisation GetOrganisationByName(string name)
-        //{
-        //    using (var db = new Context())
-        //    {
-        //        var organisation = db.Organisations
-        //            .Where(organisation => organisation.OrgName == name)
-        //            .Include(x => x.OrgType)
-        //            .Include(x => x.Locality)
-        //            .Single();
-        //        if (organisation == null)
-        //            throw new ArgumentException($"Нет организации с названием \"{name}\"");
-        //        return organisation;
-        //    }
-        //}
+        public (List<Organisation>, int) GetOrganisationsByAdressRegForOperatorVetService(
+           string adressReg, int pageNumber,
+           int pageSize, string sortBy, bool isAscending) =>
+           GetOrganisationsByValueForOperatorVetService(org => org.AdressReg.Contains(adressReg, StringComparison.InvariantCultureIgnoreCase),
+               pageNumber, pageSize, sortBy, isAscending);
 
-        //public static List<Organisation> GetOrganisationsByName(string name)
-        //{
-        //    using (var db = new Context())
-        //    {
-        //        var organisations = db.Organisations
-        //            .Where(organisation => organisation.OrgName.Contains(name))
-        //            .Include(x => x.OrgType)
-        //            .Include(x => x.Locality);
-        //        //if (organisations.Count() == 0)
-        //        //    throw new ArgumentException($"Нет организаций с названием \"{name}\"");
-        //        return organisations.ToList();
-        //    }
-        //}
-
-        //public static List<Organisation> GetOrganisationsByINN(string inn)
-        //{
-        //    using (var db = new Context())
-        //    {
-        //        var organisations = db.Organisations
-        //            .Where(organisation => organisation.INN.Contains(inn))
-        //            .Include(x => x.OrgType)
-        //            .Include(x => x.Locality);
-        //        //if (organisations.Count() == 0)
-        //        //    throw new ArgumentException($"Нет организаций с названием \"{name}\"");
-        //        return organisations.ToList();
-        //    }
-        //}
-
-        //public static List<Organisation> GetOrganisationsByKPP(string kpp)
-        //{
-        //    using (var db = new Context())
-        //    {
-        //        var organisations = db.Organisations
-        //            .Where(organisation => organisation.KPP.Contains(kpp))
-        //            .Include(x => x.OrgType)
-        //            .Include(x => x.Locality);
-        //        //if (organisations.Count() == 0)
-        //        //    throw new ArgumentException($"Нет организаций с названием \"{name}\"");
-        //        return organisations.ToList();
-        //    }
-        //}
-
+        public (List<Organisation>, int) GetOrganisationsByAdressRegForOperatorOMSU(
+           string adressReg, int pageNumber,
+           int pageSize, string sortBy, bool isAscending) =>
+           GetOrganisationsByValueForOperatorOMSU(org => org.AdressReg.Contains(adressReg, StringComparison.InvariantCultureIgnoreCase),
+               pageNumber, pageSize, sortBy, isAscending);
     }
 
     static class OrganisationExtension
