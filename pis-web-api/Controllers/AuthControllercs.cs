1251cf094cce10c06ccecc0476cebd428eb6d48f
MKstii <101364255+MKstii@users.noreply.github.com>
2023-11-25 16:15:37 +0500

    Насрал и починил

--- pis-web-api/Controllers/AuthControllercs.cs
+++ pis-web-api/Controllers/AuthControllercs.cs
@@ -29,8 +29,8 @@ namespace pis_web_api.Controllers
             try
             {
                 var user = _userService.LoginUser(login, password);
-                var userPost = new UserPost();
-                return Ok(user);
+                var userPost = user.ConvertToUserPost();
+                return Ok(userPost);
             }
             catch (Exception ex)
             {
