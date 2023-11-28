6d687d9afdc93956933d8be8ca7d45aa83f6c6a8
stud0000254289 <stud0000254289@study.utmn.ru>
2023-11-28 22:34:08 +0500

    journal

---
+++ pis-web-api/Models/db/Journal.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pis_web_api.Models.db
+{
+    public class Journal
+    {
+        [Key]
+        public int JounalID { get; set; } 
+        public int UserID { get; set; }
+        public User User { get; set; }
+        public int EditID { get; set; }
+        public string DescriptionObject { get; set; }
+        public string TableName { get; set; }
+
+
+        public Journal(int userID, int editID, string descriptionObject, string tableName)
+        {
+            UserID = userID;
+            EditID = editID;
+            DescriptionObject = descriptionObject;
+            TableName = tableName;
+        }
+    }
+}
